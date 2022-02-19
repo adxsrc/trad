fae63dbb8479aa3fe2c2a429d8794b80de1bc431
Chi-kwan Chan <chanc@arizona.edu>
2022-02-18 23:32:16 -0700

    Add docstrings to top level package and tests

--- mod/trad/__init__.py
+++ mod/trad/__init__.py
@@ -11,3 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+
+"""Composable Radiative Transfer"""
+
+
+__version__ = "0.0.0"
