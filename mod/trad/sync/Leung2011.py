380f05fd73735af6b4cebaf1db37f1da14a725d9
Chi-kwan Chan <chanc@arizona.edu>
2023-07-17 22:55:32 -0700

    Merge emissivity() and absorptivity() into coefficients() in Leung2011

--- mod/trad/sync/Leung2011.py
+++ mod/trad/sync/Leung2011.py
@@ -22,13 +22,14 @@ from ..specradiance import blackbody
 
 
 @phun({
-    'si' : (u.W      ) / u.sr / u.m**3  / u.Hz,
-    'cgs': (u.erg/u.s) / u.sr / u.cm**3 / u.Hz,
+    'si' : ((u.W      ) / u.sr / u.m**3  / u.Hz, u.m **-1),
+    'cgs': ((u.erg/u.s) / u.sr / u.cm**3 / u.Hz, u.cm**-1),
 })
-def emissivity(u_nu, u_ne, u_Te, u_B, u_theta, u_res='si', backend=None, pol=False):
-    r"""Synchrotron emissivity
+def coefficients(u_nu, u_ne, u_Te, u_B, u_theta, u_res='si', backend=None, pol=False):
+    r"""Synchrotron emissivity and absorptivity
 
-    An approximation of the synchrotron emissivity at given
+    An approximation of the synchrotron emissivity and absorptivity at
+    given
     frequency               :math:`\nu`,
     electron number density :math:`n_e`,
     electron temperature    :math:`T_e`,
@@ -37,21 +38,26 @@ def emissivity(u_nu, u_ne, u_Te, u_B, u_theta, u_res='si', backend=None, pol=Fal
     derived by Leung et al. (2011):
 
     .. math::
-        j_\nu = n_e
+        j_\nu
+        &= n_e
         \frac{\sqrt{2}\pi e^2 \nu_\mathrm{s}}{3 K_2(1/\Theta_e)c}
-        (X^{1/2} + 2^{11/12} X^{1/6})^2 \exp(-X^{1/3}),
+        (X^{1/2} + 2^{11/12} X^{1/6})^2 \exp(-X^{1/3}), \\
+        \alpha_\nu
+        &= j_\nu / B_\nu
 
     where
     :math:`\Theta_e = k_\mathrm{B}T_e / m_e c^2` is the dimensionless
     electron temperature,
     :math:`\nu_\mathrm{s} = (2/9)\nu_\mathrm{c}\Theta_e^2` is a
-    synchrotron characteristic frequency, and
-    :math:`X = \nu/\nu_\mathrm{s}`,
-    with :math:`k_\mathrm{B}`, :math:`m_e`, :math:`e`, :math:`c`,
-    :math:`K_2`, and :math:`\nu_\mathrm{c} = eB/2\pi m_e` being the
-    Boltzmann constant, electron mass, electron charge, speed of
-    light, modified Bessel function of the second kind of order 2, and
-    the electron cyclotron frequency, respectively.
+    synchrotron characteristic frequency,
+    :math:`X = \nu/\nu_\mathrm{s}` is a scaled frequency, and
+    :math:`\nu_\mathrm{c} = eB/2\pi m_e` is the electron cyclotron
+    frequency.
+    The symbols :math:`k_\mathrm{B}`, :math:`m_e`, :math:`e`,
+    :math:`c`, :math:`K_2`, and :math:`B_\nu` are the Boltzmann
+    constant, electron mass, electron charge, speed of light, modified
+    Bessel function of the second kind of order 2, and the Planck's
+    law, respectively.
 
     """
     if pol:
@@ -62,12 +68,15 @@ def emissivity(u_nu, u_ne, u_Te, u_B, u_theta, u_res='si', backend=None, pol=Fal
     exp = backend.exp
     sin = backend.sin
     nuc = gyrofrequency(u_B)
+    Bnu = blackbody(u_nu, u_Te)
+
+    assert u_res[0] / Bnu.unit == u_res[1]
 
     r = float(u_theta.to(u.rad))
     t = float(u_T_me.to(u_Te))
 
     s = float((2/9) / t**2)
-    A = float(2**0.5 * (pi/3) * (c.cgs.e.gauss**2/c.c/u.sr) * u_ne * nuc.unit / u_res)
+    A = float(2**0.5 * (pi/3) * (c.cgs.e.gauss**2/c.c/u.sr) * u_ne * nuc.unit / u_res[0])
     x = float(1 * u_nu / nuc.unit)
 
     def pure(nu, ne, Te, B, theta):
@@ -75,29 +84,7 @@ def emissivity(u_nu, u_ne, u_Te, u_B, u_theta, u_res='si', backend=None, pol=Fal
         X = x * nu / nus
         Y = (X**(1/2) + 2**(11/12) * X**(1/6))**2 * exp(-X**(1/3))
         K = kn(2, t/Te)
-        return A * (ne*nus) * (Y/K)
+        j = A * (ne*nus) * (Y/K)
+        return j, j/Bnu(nu, Te)
 
     return pure
-
-
-@phun({
-    'si' : 1/u.m,
-    'cgs': 1/u.cm,
-})
-def absorptivity(u_nu, u_ne, u_Te, u_B, u_theta, u_res='si', backend=None, pol=False):
-    r"""Synchrotron absorptivity"""
-
-    Bnu = blackbody(u_nu, u_Te)
-    jnu = emissivity(u_nu, u_ne, u_Te, u_B, u_theta, pol=pol)
-
-    def pure(nu, ne, Te, B, theta):
-        j = jnu(nu, ne, Te, B, theta)
-        B = Bnu(nu, Te)
-        return j / B
-
-    def purepol(nu, ne, Te, B, theta):
-        j = jnu(nu, ne, Te, B, theta)
-        B = Bnu(nu, Te)
-        return tuple(jS / B for jS in j)
-
-    return purepol if pol else pure
