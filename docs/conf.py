8b2f1e9d6a500310a3287cabb86bd9e5bccabeb4
Chi-kwan Chan <chanc@arizona.edu>
2023-07-18 22:45:01 -0700

    Bump version to 0.1.2

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Chi-kwan Chan'
 author = 'Chi-kwan Chan'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.1'
+release = '0.1.2'
 
 
 # -- General configuration ---------------------------------------------------
--- mod/trad/__init__.py
+++ mod/trad/__init__.py
@@ -16,4 +16,4 @@
 """trad: composable radiative transfer."""
 
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
